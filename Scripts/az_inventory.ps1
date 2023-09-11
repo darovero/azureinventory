50428aff2981cbddaae0870fe483c540d109c0c6
David Velasquez Rojas <drvelasquezr@outlook.com>
2023-09-11 17:43:24 -0500

    change az_inventory script

--- Scripts/az_inventory.ps1
+++ Scripts/az_inventory.ps1
@@ -4,18 +4,10 @@ param(
     [string]$outputFilePath
 )
 
-Write-Host "resourceGroup: $resourceGroup"
-Write-Host "tagsToFilter: $tagsToFilter"
-Write-Host "outputFilePath: $outputFilePath"
-
-
-# Convertir la cadena de tags a una lista de tags
 $selectedTags = $tagsToFilter -split ','
 
-# Obtener la lista de recursos en el grupo de recursos
 $resources = az resource list --resource-group $resourceGroup --query '[].{Name:name, ResourceType:type, ResourceGroupName:resourceGroup, Location:location, Tags:tags}' --output json | ConvertFrom-Json
 
-# Crear una lista para almacenar los resultados
 $tagRows = @()
 
 foreach ($resource in $resources) {
@@ -42,5 +34,4 @@ foreach ($resource in $resources) {
     $tagRows += $tagRow
 }
 
-# Exportar los resultados a un archivo CSV
 $tagRows | Export-Csv $outputFilePath -NoTypeInformation
