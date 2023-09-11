595b14251f8fb3676046f5329d19065aebebbd0c
David Velasquez Rojas <drvelasquezr@outlook.com>
2023-09-11 18:33:15 -0500

    change general improvements

--- Scripts/upload_to_blob.ps1
+++ Scripts/upload_to_blob.ps1
@@ -6,18 +6,13 @@ param(
     [string]$storageAccountKey
 )
 
-# Instala el módulo Az.Storage si no está instalado
 if (-not (Get-Module -Name Az.Storage -ListAvailable)) {
     Install-Module -Name Az.Storage -Force -AllowClobber
 }
-
-# Conectarse a la cuenta de almacenamiento de Azure
 $context = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
 
-# Ruta al archivo local que deseas cargar en el Blob Storage
-$localFilePath = "Resources_$resourceGroup.csv"  # Debes asegurarte de que este archivo exista en la ubicación correcta
+$localFilePath = "Resources_$resourceGroup.csv"
 
-# Cargar el archivo en el Blob Storage
 Set-AzStorageBlobContent -Context $context -Container $containerName -File $localFilePath -Blob $blobName -Force
 
-Write-Host "El archivo $localFilePath se ha cargado en el contenedor $containerName con el nombre de blob $blobName."
+Write-Host "The file $localFilePath has been uploded to the $containerName container with the name blob $blobName."
